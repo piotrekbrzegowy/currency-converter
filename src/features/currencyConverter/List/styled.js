7b17169e59e54edc820cf1ed695de35405c7c18a
Piotrek Brzegowy <brzegowypiotrek@gmail.com>
2022-05-20 14:59:18 +0200

    added heading and content

--- src/features/currencyConverter/List/styled.js
+++ src/features/currencyConverter/List/styled.js
@@ -5,13 +5,22 @@ export const List = styled.ul`
     margin: 0;
     padding: 0;
     word-break: break-word;
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
 `;
+
+export const Heading = styled.p`
+
+`
+export const Content = styled.span``;
 
 export const Item = styled.li`
     grid-gap: 10px;
     align-items: center;
     padding: 10px;
     border-bottom: 1px solid #ddd;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+`;
+
+export const Button = styled.button`
+    padding: 10px;
 `;

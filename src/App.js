519e6ecf0abe2da37e2ce6934649b2c13b47f87c
Piotrek Brzegowy <brzegowypiotrek@gmail.com>
2022-05-21 21:13:01 +0200

    added theme provider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 import { CurrencyConverter } from './features/currencyConverter';
 import { GlobalStyle } from './GlobalStyle';
+import { theme } from './theme';
 
 function App() {
   return (
     <>
-      <GlobalStyle />
-      <CurrencyConverter />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <CurrencyConverter />
+      </ThemeProvider>
     </>
   );
 }
---
+++ src/theme.js
@@ -0,0 +1,5 @@
+export const theme = {
+    breakpoints: {
+        mobileMax: 767,
+    }
+}

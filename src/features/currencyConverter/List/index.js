2b0a1037cc1fc6d46fcf17a4355b389c60d7d4c7
Piotrek Brzegowy <brzegowypiotrek@gmail.com>
2022-05-21 16:48:17 +0200

    calculate sum function added

--- src/features/currencyConverter/List/index.js
+++ src/features/currencyConverter/List/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { removeTransaction, selectTransactions } from "../Form/transactionSlice";
 import { Item, List, Button, Heading, Content } from "./styled";
@@ -5,6 +6,29 @@ import { Item, List, Button, Heading, Content } from "./styled";
 export const TransactionsList = () => {
     const transactions = useSelector(selectTransactions);
     const dispatch = useDispatch();
+    const [sumResult, setSumResult] = useState("");
+    const [sumAmount, setSumAmount] = useState("");
+
+    const calculateSum = () => {
+        let resultSum = 0;
+        let amountSum = 0;
+
+        transactions.forEach(element => {
+            resultSum += element.result;
+            amountSum += element.amount;
+        });
+        if (transactions.rate === "NaN" || transactions.rate === "") {
+            setSumResult(0);
+            setSumAmount(+amountSum);
+        } else {
+            setSumResult(+resultSum);
+            setSumAmount(+amountSum);
+        }
+    }
+
+    useEffect(() => {
+        calculateSum();
+    }, [calculateSum])
 
     return (
         <List>
@@ -20,10 +44,19 @@ export const TransactionsList = () => {
                         <Content>{transaction.name}</Content>
                         <Content>{transaction.amount}</Content>
                         <Content>{transaction.result}</Content>
-                        <Button onClick={() => dispatch(removeTransaction(transaction.transactionId))}>DEL</Button>
+                        <Button onClick={() => {
+                            dispatch(removeTransaction(transaction.transactionId))
+                            calculateSum()
+                        }}>DEL</Button>
                     </Item>
                 </>
             ))}
+            <Item>
+                <Heading>SUM:</Heading>
+                <Heading>{sumAmount}</Heading>
+                <Heading>{sumResult}</Heading>
+                <Heading></Heading>
+            </Item>
         </List>
 
     )

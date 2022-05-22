577379e8f9776e9c1b8ea76ad6772a3734258f31
Piotrek Brzegowy <brzegowypiotrek@gmail.com>
2022-05-22 18:11:55 +0200

    fetching rates saga function created

---
+++ src/features/currencyConverter/Form/exchangeRatesSaga.js
@@ -0,0 +1,21 @@
+import { delay, call, put, takeLatest } from "redux-saga/effects";
+import { getRate } from "./exchangeRatesAPI";
+import { fetchRates, fetchRatesError, fetchRatesSuccess } from "./exchangeRatesSlice";
+
+function* fetchRatesHandler() {
+    const exchangeRateAPIBaseUrl = "https://api.exchangerate.host/latest?base=";
+    const currency = "EUR"
+    const exchangeRatePath = `${exchangeRateAPIBaseUrl}${currency}`;
+
+    try {
+        yield delay(1000) //just to show loader
+        const exchangeRate = yield call(getRate, exchangeRatePath);
+        yield put(fetchRatesSuccess(exchangeRate));
+    } catch (error) {
+        yield put(fetchRatesError());
+    };
+}
+
+export function* watchFetchRates() {
+    yield takeLatest(fetchRates.type, fetchRatesHandler);
+}

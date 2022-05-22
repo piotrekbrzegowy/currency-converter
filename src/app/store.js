27127111237d0dfa78a494dde58e3afce08bbf98
Piotrek Brzegowy <brzegowypiotrek@gmail.com>
2022-05-22 18:11:10 +0200

    added root saga and rates reducer to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
+import createSagaMiddleware from "@redux-saga/core";
+import exchangeRatesSlice from '../features/currencyConverter/Form/exchangeRatesSlice';
 import transactionSlice from '../features/currencyConverter/Form/transactionSlice';
+import rootSaga from '../rootSaga';
+
+const sagaMiddleware = createSagaMiddleware();
 
 export const store = configureStore({
   reducer: {
     transactions: transactionSlice,
+    rates: exchangeRatesSlice,
   },
+  middleware: [sagaMiddleware],
 });
+
+sagaMiddleware.run(rootSaga);
